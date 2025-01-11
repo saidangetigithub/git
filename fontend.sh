015d66e0565234e8873edc5789d40c31b0330f5c
saidangeti <saidangeti1443@gmail.com>
2025-01-11 21:30:25 +0530

    added variablrs

--- fontend.sh
+++ fontend.sh
@@ -1,19 +1,26 @@
+log_file= /tmp/expense.logs
+component= frontend
 
-dnf install nginx -y >> /tmp/expense.logs
+echo "installing nginx"
+dnf install nginx -y >> $log_file
 
 
 echo "copy configuration files"
-cp expense.conf /etc/nginx/default.d/expense.conf >> /tmp/expense.logs
+cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
 
 
-
+echo "removing the default nginx content"
 rm -rf /usr/share/nginx/html/* 
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/expense.logs
+curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
 
 cd /usr/share/nginx/html
- 
-unzip /tmp/frontend.zip >> /tmp/expense.logs
+
+echo "extracting the zipfiles"
+unzip /tmp/$component.zip >> $log_file
+
 systemctl enable nginx 
+
+echo "starting the nginx service"
 systemctl start nginx 
 systemctl restart nginx 
