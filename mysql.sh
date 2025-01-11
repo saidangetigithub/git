cf33ed29a9d53f998f64d62a5166996cfc688dfe
saidangeti <saidangeti1443@gmail.com>
2025-01-11 23:01:39 +0530

    added mysql1

--- mysql.sh
+++ mysql.sh
@@ -3,4 +3,4 @@ sudo dnf install mysql80-community-release-el9-1.noarch.rpm  -y
 sudo dnf install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld  
-mysql_secure_installation --set-root-pass ExpenseApp@1
+#mysql_secure_installation --set-root-pass ExpenseApp@1
