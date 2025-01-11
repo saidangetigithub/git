8f4fa92b387fa52118ed8355a84baeab3446300d
saidangeti <saidangeti1443@gmail.com>
2025-01-11 22:17:17 +0530

    added changes

--- fontend.sh
+++ fontend.sh
@@ -1,23 +1,23 @@
-log_file= "/tmp/expense.logs"
-component= "frontend"
+log_file=/tmp/expense.logs
+component=frontend
 
 echo "installing nginx"
-dnf install nginx -y >> $log_file
+dnf install nginx -y >>$log_file
 
 
 echo "copy configuration files"
-cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
+cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
 
 
 echo "removing the default nginx content"
 rm -rf /usr/share/nginx/html/* 
 
-curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
+curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
 
 cd /usr/share/nginx/html
 
 echo "extracting the zipfiles"
-unzip /tmp/$component.zip >> $log_file
+unzip /tmp/$component.zip >>$log_file
 
 systemctl enable nginx 
 
